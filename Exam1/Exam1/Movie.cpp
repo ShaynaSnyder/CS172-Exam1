dc8b33f4fc38ca3fd8aed0a008c76dfeb62ceed4
ssnyder19 <ssnyder19@LAB-SCI-214-05>
2016-10-06 13:24:22 -0700

    Possibly done with movie files

--- Exam1/Exam1/Movie.cpp
+++ Exam1/Exam1/Movie.cpp
@@ -1,1 +1,31 @@
-#include "Movie.h"
+#include <string>
+#include "Movie.h" //including Movie header file
+using namespace std;
+
+//constructs default movie object
+Movie::Movie()
+{
+	movieTitle = "Star Wars: The Force Awakens";
+	movieGenre = "Comedy";
+	movieShowtime = 3;
+}
+//constructs Movie object using string and int variables
+Movie::Movie(string title, string genre, int showtime)
+{
+	movieTitle = title;
+	if(genre=="Action" || genre=="Horror" || genre=="Documentary")
+		movieGenre = genre;
+	movieShowtime = showtime;
+}
+string Movie::getTitle() //get function gets movie title
+{
+	return movieTitle;
+}
+string Movie::getGenre() //get function gets movie genre
+{
+	return movieGenre;
+}
+int Movie::getShowtime() //get function gets movie showtime
+{
+	return movieShowtime;
+}
--- Exam1/Exam1/Movie.h
+++ Exam1/Exam1/Movie.h
@@ -5,12 +5,13 @@
 //creates Movie class
 class Movie
 {
-private:
-
-public:
+private: //declares movie title, genre, and showtime as private variables
+	string movieTitle, movieGenre;
+	int movieShowtime;
+public: //declares functions as public methods
 	Movie(); //Default movie for theater
 	//Genres include: Action, Comedy, Horror, and Documentary. Default: Comedy
-	Movie(string title, string genre, int showTime);
+	Movie(string title, string genre, int showtime);
 	string getTitle(); //Returns movie title
 	string getGenre(); //Returns movie genre
 	int getShowtime(); //Returns movie show time
